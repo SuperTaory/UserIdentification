46a80d97f65becb6d03e4caecd5935889531c739
zhangliutao <884534791@qq.com>
2019-10-29 16:50:29 +0800

    统计个体MacInfo

--- src/main/Scala/MacCompression.scala
+++ src/main/Scala/MacCompression.scala
@@ -27,12 +27,12 @@ object MacCompression {
     //    macFile.createOrReplaceTempView("MacTable")
     macFile.registerTempTable("MacTable")
     // 1559318400是指2019/06/01 00:00:00对应的unix时间戳
-    val macRDD = sq.sql("select * from MacTable where " + macSchema.fieldNames(1) + " > 1559318400").rdd
+    val macRDD = sq.sql("select * from MacTable").rdd
 
     val transformedMacRDD = macRDD.map(line => {
       val fields = line.toString().split(',')
       val macId = fields(0).drop(1)
-      val time = fields(1)
+      val time = fields(1).toLong
       val station = fields(2)
       (macId, time, station)
     }).sortBy(line => (line._1, line._2), ascending = true)
@@ -41,23 +41,33 @@ object MacCompression {
 
 
     // 过滤掉同一站点相邻时刻重复检测到的mac信息
-    var baseTime = "0"
+    var baseTime = 0L
     var baseStation = "null"
     val limitTime = 600 // 10min = 600sec
-    val compressionRDD = transformedMacRDD.filter(line =>{
-      if (line._3 == baseStation && line._2.toInt < baseTime.toInt + limitTime)
+    val compressionRDD = transformedMacRDD.filter(line => {
+      if (line._3 == baseStation && line._2 < baseTime + limitTime) {
+        baseTime = line._2
         false
-      else{
+      } else{
         baseStation = line._3
         baseTime = line._2
         true
       }
-    }).map(line => {
-      val time = transTimeToString(line._2)
-      (line._1, time, line._3)
-    })
-
+    }).cache()
     compressionRDD.saveAsTextFile(args(1))
+
+    // 将时间个数从时间戳转换为时间字符串
+    val changeTimeFormat = compressionRDD.map(line => {
+      (line._1, transTimeToString(line._2.toString), line._3)
+    })
+    changeTimeFormat.saveAsTextFile(args(2))
+
+//    val personalMacRDD = compressionRDD.map(line => {
+//      val time = transTimeToString(line._2.toString)
+//      (line._1, time + '/' + line._3)
+//    }).groupByKey().mapValues(v => v.toList.sorted.reduce(_ + ',' + _))
+//    personalMacRDD.saveAsTextFile(args(1))
+
     sc.stop()
   }
 
