10b5c06e26427e27a451afeeca8b0111db036ab1
zhangliutao <884534791@qq.com>
2019-12-19 23:15:39 +0800

    读取Parquet文件

---
+++ src/main/Scala/ReadParquet.scala
@@ -0,0 +1,32 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
+
+object ReadParquet {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("ReadParquet")
+    val sc = new SparkContext(conf)
+    // val sq = SparkSession.builder().config(conf).getOrCreate()
+    val sq = new SQLContext(sc)
+
+    // 读取mac数据并转换为数据库Table格式
+    val macFile = sq.read.parquet(args(0))
+    macFile.printSchema()
+//    val macSchema = macFile.schema
+    //    macFile.createOrReplaceTempView("MacTable")
+    macFile.registerTempTable("MacTable")
+    // 1559318400是指2019/06/01 00:00:00对应的unix时间戳
+    val macRDD = sq.sql("select * from MacTable").rdd
+
+    val transformedMacRDD = macRDD.map(line => {
+      val fields = line.toString().split(',')
+      val macId = fields(0).drop(1)
+      val time = fields(1).toLong
+      val station = fields(2)
+      val macCount = fields.last.dropRight(1).toInt
+      (macId, time, station, macCount)
+    }).sortBy(line => (line._1, line._2), ascending = true)
+
+    transformedMacRDD.saveAsTextFile(args(1))
+    sc.stop()
+  }
+}
