53a110c850dda2425971a48717a11a5b74121232
zhangliutao <884534791@qq.com>
2019-12-19 23:13:45 +0800

    常用函数集合

---
+++ src/main/Scala/GeneralFunctionSets.scala
@@ -0,0 +1,41 @@
+import java.text.SimpleDateFormat
+import java.util.{Calendar, TimeZone}
+
+object GeneralFunctionSets {
+  def transTimeToTimestamp(timeString : String) : Long = {
+    val pattern = "yyyy-MM-dd HH:mm:ss"
+    val dateFormat = new SimpleDateFormat(pattern)
+    dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"))
+    val time = dateFormat.parse(timeString).getTime / 1000
+    time
+  }
+
+  def transTimeToString(timeStamp : Long) : String = {
+    val pattern = "yyyy-MM-dd HH:mm:ss"
+    val dateFormat = new SimpleDateFormat(pattern)
+    dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"))
+    val time = dateFormat.format(timeStamp * 1000)
+    time
+  }
+
+  def dayOfMonth_string(timeString : String) : Int = {
+    val pattern = "yyyy-MM-dd HH:mm:ss"
+    val dateFormat = new SimpleDateFormat(pattern)
+    dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"))
+    val time = dateFormat.parse(timeString)
+    val calendar = Calendar.getInstance()
+    calendar.setTime(time)
+    calendar.get(Calendar.DAY_OF_MONTH)
+  }
+
+  def dayOfMonth_long(t : Long) : Int = {
+    val pattern = "yyyy-MM-dd HH:mm:ss"
+    val dateFormat = new SimpleDateFormat(pattern)
+    dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"))
+    val timeString = dateFormat.format(t * 1000)
+    val time = dateFormat.parse(timeString)
+    val calendar = Calendar.getInstance()
+    calendar.setTime(time)
+    calendar.get(Calendar.DAY_OF_MONTH)
+  }
+}
